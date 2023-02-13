41d61685b96dd0f7fe7039bb52c13a3fb838cb70
Noura Algohary <noura.malgohary@gmail.com>
2023-02-13 13:01:27 +0200

    add 1523

---
+++ 1523. Count Odd Numbers in an Interval Range.py
@@ -0,0 +1,8 @@
+# no. of odd numbers between 2 numbers is (high - low) / 2
+# if one of them is odd, then it counts
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if(low % 2 != 0 or high % 2 != 0):
+            return int((high - low) / 2) + 1
+        else:
+            return int((high - low) / 2)
